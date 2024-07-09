07dac1f4ff36389b69c591bded381c4b8799d434
ubuntu <hjkja0511@gmail.com>
2024-07-09 08:36:54 +0000

    feat: add n6gw

--- lab3/ans/config/upf-iptables.sh
+++ lab3/ans/config/upf-iptables.sh
@@ -3,7 +3,7 @@
 # Configure iptables in UPF
 #
 ip route delete default via 10.100.3.1
-ip route add default via 10.100.6.1 dev dn0
+ip route add default via 10.100.6.101 dev dn0
 
 iptables -t nat -A POSTROUTING -o dn0  -j MASQUERADE
 iptables -I FORWARD 1 -j ACCEPT
